f9b658b55f695100b15c8245f654a9a40f7fae8a
maidzui123 <maiduy190802@gmail.com>
2023-07-10 15:24:55 +0700

    upd1

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+// import ButtonBoostrap from './components/Button';
+// import NavBar from './components/NavBar';
+function App() {
+  let style = {
+    backgroundColor: 'aqua'
+  }
+  return (
+    <div style={style}>
+      Hello World
+      <div htmlFor="agag">For</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function ButtonBoostrap() {
+    return <Button variant="primary">Hello, React Bootstrap!</Button>;
+}
+
+export default ButtonBoostrap;
