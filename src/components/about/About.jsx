8fd01f34207146ec00c423964506edf0a9767e40
maidzui123 <maiduy190802@gmail.com>
2023-07-12 16:40:57 +0700

    upd4

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/images/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaSchool} from 'react-icons/fa'
+import {FaComputer} from 'react-icons/fa6'
+
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className='container about__container'>
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='About Image' />
+          </div>
+        </div>  
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaComputer className='about-icon'/>
+              <h5>Major</h5>
+              <small>Software Engineer</small>
+            </article>
+
+            <article className='about__card'>
+              <FaSchool className='about-icon'/>
+              <h5>School</h5>
+              <small>Ton Duc Thang University</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about-icon'/>
+              <h5>Experience</h5>
+              <small>1 Year Working</small>
+            </article>
+          </div>
+          <p>
+            I am a software engineer with a passion for creating websites and web applications.
+            I have experience working with HTML, CSS, JavaScript, React, Node.js, Express, MongoDB, MySQL, and PostgreSQL.
+            I have experience working with Node.js, Express, MongoDB, MySQL, and PostgreSQL.
+          </p>
+          <a href='#contact' className='btn btn-primary'></a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,7 +6,7 @@ const HeaderSocials = () => {
   return (
     <div className="header__socials">
         <a href="https://www.facebook.com/profile.php?id=100008189433403" target="_blank"><BsFacebook size={22}/></a>
-        <a href="https://www.instagram.com/_mzuyy/" target="_blank"><AiFillInstagram size={22}/></a>
+        <a href="https://www.instagram.com/_mzuyy/" target="_blank"><AiFillInstagram size={25}/></a>
         <a href="https://github.com/maidzui123" target="_blank"><FaGithub size={22}/></a>
     </div>
   )
