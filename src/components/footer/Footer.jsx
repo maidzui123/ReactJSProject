bd6974ab8a767b4cf53e0015a9c40380d981adf7
maidzui123 <maiduy190802@gmail.com>
2023-07-15 01:21:21 +0700

    upd6

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {FiGithub} from 'react-icons/fi'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Mynd</a>
+
+      <ul className='personal__link'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/profile.php?id=100008189433403"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/_mzuyy/"><FiInstagram/></a>
+        <a href="https://github.com/maidzui123"><FiGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Mynd. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ const CTA = () => {
   return (
     <div className="cta">
         <a href={CV} download className='btn'>Dowload My CV</a>
-        <a href="#talk" className='btn btn-primary'>Let's Talk</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
     </div>
   )
